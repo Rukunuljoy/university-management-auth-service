91bdb048dcb5a96c06b2bf045d091ca51120a991
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-05-30 19:49:36 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function tailwind() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("database connection established");
+    await mongoose.connect(config.database_url as string)
+    console.log('database connection established')
 
     app.listen(config.port, () => {
-      console.log(`application listening on port ${config.port}`);
-    });
+      console.log(`application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("failed to connect", error);
+    console.log('failed to connect', error)
   }
 }
 
-tailwind();
+tailwind()
